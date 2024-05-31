45ed70c4f5b8f9989f4ebc2d41fd3433bff3ba6d
GOOD • BOY <154020288+D4X-UMAR@users.noreply.github.com>
2024-05-31 10:34:04 +0500

    GOLD MD

--- commands/Warn.js
+++ commands/Warn.js
@@ -1,13 +1,12 @@
 /** 
 
-🇫‌🇱‌🇦‌🇸‌🇭‌-🇲‌🇩‌ 
-
+GOLD-MD
   𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
  𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
  𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
  𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
- * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙𝗹𝗮𝘀𝗵 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
- * @𝗼𝘄𝗻𝗲𝗿: 𝗙𝗿𝗮𝗻𝗰𝗲 𝗞𝗶𝗻𝗴 
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : GOLD 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: UMAR 
  
  **/
 
@@ -30,7 +29,7 @@ france(
     },async (dest,zk,commandeOptions) => {
 
  const {ms , arg, repondre,superUser,verifGroupe,verifAdmin , msgRepondu , auteurMsgRepondu} = commandeOptions;
-if(!verifGroupe ) {repondre('this is a group commands') ; return};
+if(!verifGroupe ) {repondre('Yeh command sirf groups me use karo') ; return};
 
 if(verifAdmin || superUser) {
    if(!msgRepondu){repondre('Ap kis Bande ko warnig dena chahte hai usko mention karo'); return};
@@ -41,15 +40,15 @@ if(verifAdmin || superUser) {
    let warnlimit = s.WARN_COUNT
    
    if( warn >= warnlimit ) { await repondre('Is bande ki warnigs khatam hui , to mene remove ker dya');
-                zk.groupParticipantsUpdate(dest, [auteurMsgRepondu], "remove")
+                zk.groupParticipantsUpdate(dest, [auteurMsgRepondu], "kick")
  } else { 
 
     var rest = warnlimit - warn ;
-     repondre(`this user is warned , rest before kick : ${rest} `)
+     repondre(`Is member ko warning mil gayi hai baki warning reh gayi : ${rest} `)
    }
 } else if ( arg[0] === 'reset') { await resetWarnCountByJID(auteurMsgRepondu) 
 
-    repondre("Is Bande ki warnigs reset ho gayi hai")} else ( repondre('reply to a user by typing  .warn ou .warn reset'))
+    repondre("Is Bande ki warnigs reset ho gayi hai")} else ( repondre('Jis member ko ap ne pehle warning di thi usko tag ya mention karo likho .warn ou .warn reset'))
    
 }  else {
     repondre('Yeh cmnd sirf group admins use kar sakte hai ap admin nahi ho')
