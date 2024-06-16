10d87804f941cf6dd0dd8244ced6e4008b9c40ca
GOOD • BOY <154020288+D4X-UMAR@users.noreply.github.com>
2024-06-16 06:48:25 +0500

    GOLD MD

---
+++ set.js
@@ -0,0 +1,56 @@
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || 'GOLD-MD',
+    PREFIXE: process.env.PREFIX || "+",
+    OWNER_NAME: process.env.OWNER_NAME || "UMAR",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "233201817959", 
+             
+    AUTO_VIEW_STATUS: process.env.AUTO_VIEW_STATUS || "on",
+AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "off",
+CHATBOT: process.env.CHAT_BOT || "off",
+    AUTO_SAVE_STATUS: process.env.AUTO_SAVE_STATUS || 'off',
+    BOT : process.env.BOT_NAME || 'GOLD-MD',
+    OPENAI_API_KEY : process.env.OPENAI_API_KEY || '',
+    URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/3f212a58fef59cab3ac7c.jpg',
+    MODE: process.env.MODE || "private",
+    PM_PERMIT: process.env.PM_PERMIT || 'no',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
+    HEROKU_APY_KEY : process.env.HEROKU_API_KEY ,
+    WARN_COUNT : process.env.WARN_COUNT || '2' ,
+    PRESENCE : process.env.PRESENCE || '',
+    //GPT : process.env.OPENAI_API_KEY || '',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'yes',
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+    /* new Sequelize({
+     dialect: 'sqlite',
+     storage: DATABASE_URL,
+     logging: false,
+})
+: new Sequelize(DATABASE_URL, {
+     dialect: 'postgres',
+     ssl: true,
+     protocol: 'postgres',
+     dialectOptions: {
+         native: true,
+         ssl: { require: true, rejectUnauthorized: false },
+     },
+     logging: false,
+}),*/
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
